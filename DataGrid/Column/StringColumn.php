594453c7746a444c585a4aa7d322f0b53b2470ea
iamluc <luc@vieillescazes.net>
2014-06-18 18:11:12 +0200

    Limit string length

--- DataGrid/Column/StringColumn.php
+++ DataGrid/Column/StringColumn.php
@@ -4,6 +4,43 @@ namespace Datatheke\Bundle\PagerBundle\DataGrid\Column;
 
 class StringColumn extends AbstractColumn
 {
+    protected $maxLength;
+    protected $moreText = '...';
+
+    public function setMaxLength($maxLength)
+    {
+        $this->maxLength = (int) $maxLength;
+
+        return $this;
+    }
+
+    public function getMaxLength()
+    {
+        return $this->maxLength;
+    }
+
+    public function setMoreText($moreText)
+    {
+        $this->moreText = $moreText;
+
+        return $this;
+    }
+
+    public function getMoreText()
+    {
+        return $this->moreText;
+    }
+
+    public function format($value)
+    {
+        $value = $this->field->formatOutput($value);
+        if (null !== $this->maxLength && strlen($value) > $this->maxLength) {
+            $value = substr($value, 0, $this->maxLength).$this->moreText;
+        }
+
+        return $value;
+    }
+
     public function getType()
     {
         return 'string';
