7c15f794a8f633d2f8135199c94e1e2200d5520e
iamluc <luc@vieillescazes.net>
2014-06-18 18:10:46 +0200

    Choose True value

--- DataGrid/Column/BooleanColumn.php
+++ DataGrid/Column/BooleanColumn.php
@@ -2,8 +2,33 @@
 
 namespace Datatheke\Bundle\PagerBundle\DataGrid\Column;
 
+use Symfony\Component\Form\Extension\Core\DataTransformer\BooleanToStringTransformer;
+
 class BooleanColumn extends AbstractColumn
 {
+    protected $trueValue;
+
+    public function setTrueValue($trueValue)
+    {
+        $this->trueValue = $trueValue;
+
+        return $this;
+    }
+
+    public function getTrueValue()
+    {
+        return $this->trueValue;
+    }
+
+    public function initialize()
+    {
+        if (null !== $this->trueValue) {
+            $this->field->addDataTransformer(
+                new BooleanToStringTransformer($this->trueValue)
+            );
+        }
+    }
+
     public function getType()
     {
         return 'boolean';
